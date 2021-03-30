6b056023c3f095d7c5e8620fc084eee3fceb9d1c
dev <dev@qiyutech.tech>
2021-03-30 15:35:28 +0800

    v0.2.0

--- code_run_dt/job.py
+++ code_run_dt/job.py
@@ -3,10 +3,12 @@ from typing import Optional, Any, Literal
 
 from pydantic import BaseModel, Field
 
-__all__ = ["CodeRunJob", "CodeRunState", "JobRunState"]
+__all__ = ["CodeRunTask", "CodeRunReportMsg", "CodeRunTaskState"]
 
 
-class CodeRunJob(BaseModel):
+class CodeRunTask(BaseModel):
+    """CodeRun 任务"""
+
     job_id: str = Field(..., title="任务ID")
     lang: Literal["c", "c++", "python", "rust"] = Field(
         "c", title="编程语言", description="code 使用的编程语言"
@@ -14,24 +16,32 @@ class CodeRunJob(BaseModel):
     code: str = Field(..., title="代码")
     args: str = Field(..., title="参数")
     stdin: str = Field(..., title="标准输入")
-    # run_config 里面的配置
-    config: Optional[Any] = Field(None, title="运行配置")
+    config: Optional[Any] = Field(
+        None, title="运行配置", description="运行时配置,每种语言都不一样 参见: run_config 里面的具体配置"
+    )
 
 
-class JobRunState(str, Enum):
+class CodeRunTaskState(str, Enum):
+    """CoeRun 任务状态"""
+
     COMPILE_START = "compile_start"
     COMPILE_DONE = "compile_done"
 
     RUN_START = "run_start"
     RUN_DONE = "run_done"
 
+    # 失败状态
+    F_OK = "f_ok"
     F_COMPILE = "f_compile"
     F_RUN = "f_run"
-    F_SUCCESS = "f_success"
 
 
-class CodeRunState(BaseModel):
+class CodeRunReportMsg(BaseModel):
+    """上报信息消息"""
+
     job_id: str = Field(..., title="任务ID")
-    state: JobRunState = Field(..., title="任务状态")
+    state: CodeRunTaskState = Field(..., title="任务状态")
     stdout: str = Field("", title="标准输出")
     stderr: str = Field("", title="标准错误")
+    # fixed value
+    typ: Literal["job.report"] = Field("job.report", title="类型", alias="type")
--- code_run_dt/test_auth.py
+++ code_run_dt/test_auth.py
@@ -1,7 +1,7 @@
 from pydantic import ValidationError
 
 from .auth import AuthMsg, AuthRet
-from .job import CodeRunState
+from .job import CodeRunReportMsg
 
 __all__ = []  # 测试不需要导入
 
@@ -17,12 +17,12 @@ def test_auth_ret():
 
 
 def test_good_code_run_state():
-    v = CodeRunState(job_id="123", state="compile_start")  # noqa
+    v = CodeRunReportMsg(job_id="123", state="compile_start")  # noqa
     print(v.json())
 
 
 def test_bad_code_run_state():
     try:
-        CodeRunState(job_id="123", state="this is a bad state")  # noqa
+        CodeRunReportMsg(job_id="123", state="this is a bad state")  # noqa
     except ValidationError:
         pass
